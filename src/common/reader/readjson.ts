84798a75ff3a001c0c0331725d49e47e87c7b6b6
roach <tmdgusya@naver.com>
2021-12-19 01:04:52 +0900

    readjson Generic 을 통해서 Type 으로 변환 할 수 있게 수정

--- src/common/reader/readjson.ts
+++ src/common/reader/readjson.ts
@@ -3,15 +3,17 @@ import {readFileSync} from "fs";
 const ENCODING_TYPE = 'utf8';
 const NOT_EXIST_JSON_FILE_ERROR_MESSAGE = "JSON FILE 이 존재하지 않습니다.";
 
-export default function readJson(path: string) : string | null {
+const jsonParserUnknown = (jsonString: string) : unknown => JSON.parse(jsonString)
+
+export default function readJson<T>(path: string) : T {
 
     const jsonBuffer  = readFileSync(path, {encoding: ENCODING_TYPE})
 
-    const result = jsonBuffer.toString();
+    const result = jsonBuffer;
 
     if (!result) {
         throw Error(NOT_EXIST_JSON_FILE_ERROR_MESSAGE);
     }
 
-    return result;
+    return jsonParserUnknown(result) as T;
 }
