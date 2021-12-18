36941663fb82b2e45ee1e7a272ce0ea64574ec0c
teihong93 <teihong93@gmail.com>
2021-12-19 04:11:15 +0900

    [Chapter01:Section1] 처음 예시 코드 추가

--- src/chapter01/index.js
+++ src/chapter01/index.js
@@ -1,1 +1,5 @@
-console.log("hello 리팩따구");
+import { statement } from "./statement_original.mjs";
+import { invoices } from "./json/invoices.mjs";
+import { plays } from "./json/plays.mjs";
+
+console.log(statement(invoices[0], plays));
