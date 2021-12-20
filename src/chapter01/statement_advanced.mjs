a71362eecdca64c690d2492a905a8f30d5d7bad2
teihong93 <teihong93@gmail.com>
2021-12-21 01:47:39 +0900

    [Chapter01:Section6] statement를 만드는 부분과 렌더하는 부분으로 분리

--- src/chapter01/statement_advanced.mjs
+++ src/chapter01/statement_advanced.mjs
@@ -1,19 +1,10 @@
 export function statement(invoice, plays) {
-    let result = `청구내역 = ${invoice.customer} \n`;
-
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2,
-    }).format;
-
     const playFor = (aPerformance) => {
         return plays[aPerformance.playID];
     };
-
     const amountFor = (aPerformance) => {
         let result = 0;
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -28,7 +19,7 @@ export function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`알 수 없는 장르 ${playFor(aPerformance).type}`);
+                throw new Error(`알 수 없는 장르 ${aPerformance.play.type}`);
         }
         return result;
     };
@@ -36,35 +27,59 @@ export function statement(invoice, plays) {
     const volumeCreditsFor = (aPerformance) => {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
-        if (playFor(aPerformance).type === "comedy")
-            result += Math.floor(aPerformance.audience / 5);
+        if (aPerformance.play.type === "comedy") result += Math.floor(aPerformance.audience / 5);
 
         return result;
     };
 
-    const totalVolumeCredits = () => {
+    const totalVolumeCredits = (data) => {
         let volumeCredits = 0;
-        for (let pref of invoice.performances) {
+        for (let pref of data.performances) {
             volumeCredits += volumeCreditsFor(pref);
         }
         return volumeCredits;
     };
 
-    const totalAmount = () => {
+    const totalAmount = (data) => {
         let result = 0;
-        for (let pref of invoice.performances) {
-            result += amountFor(pref);
+        for (let pref of data.performances) {
+            result += pref.amount;
         }
         return result;
     };
 
-    for (let pref of invoice.performances) {
+    const enrichPerformance = (aPerformance) => {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
+        return result;
+    };
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+
+    return renderPlainText(statementData);
+}
+
+function renderPlainText(data) {
+    let result = `청구내역 = ${data.customer} \n`;
+
+    const format = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format;
+
+    for (let pref of data.performances) {
         // 청구 내역 출력
-        result += `${playFor(pref).name}: ${format(amountFor(pref) / 100)}: ${pref.audience}석 \n`;
+        result += `${pref.play.name}: ${format(pref.amount / 100)}: ${pref.audience}석 \n`;
     }
 
-    result += `총액 : ${format(totalAmount() / 100)}\n`;
-    result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
+    result += `총액 : ${format(data.totalAmount / 100)}\n`;
+    result += `적립 포인트 : ${data.totalVolumeCredits}점\n`;
 
     return result;
 }
