f17cddbb16b50168a380540902383b9737f96cfd
teihong93 <teihong93@gmail.com>
2021-12-22 01:36:27 +0900

    [Chapter01:Section9] 타입을 서브클래스로 분리

--- src/chapter01/create_statement_data.mjs
+++ src/chapter01/create_statement_data.mjs
@@ -1,27 +1,8 @@
+import { createPerformanceCalculator } from "./PerformanceCalculator.mjs";
+
 export function createStatementData(invoice, plays) {
     const playFor = (aPerformance) => {
         return plays[aPerformance.playID];
-    };
-    const amountFor = (aPerformance) => {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르 ${aPerformance.play.type}`);
-        }
-        return result;
     };
 
     const volumeCreditsFor = (aPerformance) => {
@@ -49,10 +30,11 @@ export function createStatementData(invoice, plays) {
     };
 
     const enrichPerformance = (aPerformance) => {
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     };
     const statementData = {};
