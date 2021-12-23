1c4ffbd4d03e05579e7ba94832a1a7f74354baf1
Dongmin Ahn <eamon3481@naver.com>
2021-12-23 22:00:37 +0900

    [Chapter01:Section4]  변수명 명확하게 짓기

--- src/chapter01/amountFor.js
+++ src/chapter01/amountFor.js
@@ -1,24 +1,27 @@
 
-    export const amountFor = (pref, play) => {
-        // 값이 변하지 않는 변수는 매개변수로 전달 perf / play
-        let thisAmount = 0;
-
+    export const amountFor = (aPerformance, play) => {
+        //  명확한 변수로 변경하기
+        // 역활이 뚜렷하지 않을때는 a/ an 을 붙인다
+        let result = 0;
+      // 더 명확한 변수로 변경하기
+      // 리턴값에는 result 명명한다.
+      
         switch (play.type) {
             case "tragedy":
-                thisAmount = 40000;
-                if (pref.audience > 30) {
-                    thisAmount += 1000 * (pref.audience - 30);
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
-                if (pref.audience > 20) {
-                    thisAmount += 10000 + 500 * (pref.audience - 20);
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
                 }
-                thisAmount += 300 * pref.audience;
+                result += 300 * aPerformance.audience;
                 break;
             default:
                 throw new Error(`알 수 없는 장르 ${play.type}`);
         }
-        return thisAmount
+        return result
 }
