170d5097df1a3d7f6a816686955b0cd865bdb179
Dongmin Ahn <eamon3481@naver.com>
2022-01-05 20:54:34 +0900

    [Chapter04:Section5] 픽스처 수정하기

--- src/chapter04/sample.test.js
+++ src/chapter04/sample.test.js
@@ -28,4 +28,10 @@ describe("province", () => {
     it('profit',function(){
         expect(asia.profit).toBe(230)
     })
+
+    it('change production',function(){
+        asia.producers[0].production = 20;
+        expect(asia.shortfall).toBe(-6);
+        expect(asia.profit).toBe(292)
+    })
 });
