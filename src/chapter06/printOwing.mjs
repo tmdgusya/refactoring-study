18c03a89ce25876ab601a4383c39a67b9a4c5efa
teihong93 <teihong93@gmail.com>
2022-01-09 03:26:17 +0900

    [Chapter06:Section1] 함수 추출하기 예제 적용

--- src/chapter06/printOwing.mjs
+++ src/chapter06/printOwing.mjs
@@ -1,20 +1,34 @@
 export function printOwing(invoice) {
-    let outstanding = 0;
+    printBanner();
+    const outstanding = calculateOutstanding(invoice);
+    recordDueDate(invoice);
+    printDetails(invoice, outstanding);
+}
 
+const printBanner = () => {
     console.log("****************");
     console.log("******채무*******");
     console.log("****************");
+};
 
-    /* 미해결 채무 계산 */
-    for (const o of invoice.orders) {
-        outstanding += o.amount;
-    }
-
-    /* 마감일 기록*/
-    const today = new Date(); // 원본에서는 Clock.today
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+const printDetails = (invoice, outstanding) => {
     /* 세부사항 출력 */
     console.log("고객명" + invoice.customer);
     console.log("채무액" + outstanding);
     console.log("마감일" + invoice.dueDate.toLocaleDateString());
-}
+};
+
+const recordDueDate = (invoice) => {
+    /* 마감일 기록*/
+    const today = new Date(); // 원본에서는 Clock.today
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+};
+
+const calculateOutstanding = (invoice) => {
+    /* 미해결 채무 계산 */
+    let result = 0;
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
+    }
+    return result;
+};
