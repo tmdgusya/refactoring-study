43c33c6228c10aad356169fbe6247362995b0d6b
teihong93 <teihong93@gmail.com>
2022-01-09 16:09:20 +0900

    [Chapter06:Section6] 주석 추가

--- src/chapter06/index.js
+++ src/chapter06/index.js
@@ -23,4 +23,9 @@ printOwing(invoice);
 const defaultOwner = getDefaultOwner();
 console.log(defaultOwner);
 
+// defaultOwner.lastName = "haha" //해당 라인은 set 이 없어 오류가 발생.
 console.log(defaultOwner);
+
+// 그런데 어떻게 해도, 자바스크립트는 객체 타입의 참조만 복사하기 때문에
+// 깊이가 깊어지면 문제가 생기게 되어있음.
+// depth가 생기는 객체들은 처음부터 깊은 복사로 데이터를 복사하던지 해야 유효.
